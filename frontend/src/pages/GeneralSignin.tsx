7b2c5ef74ae1030aaac09d627193fe6fb649ea98
Anthony Jaramillo <anthony.jaramillomartinez@gmail.com>
2023-09-07 15:09:00 -0500

    Changed hover effect

--- frontend/src/pages/GeneralSignin.tsx
+++ frontend/src/pages/GeneralSignin.tsx
@@ -15,7 +15,7 @@ const theme = createTheme({
   components: {
     MuiButton: {
       defaultProps: {
-        variant: 'contained'
+        variant: 'contained',
       },
       styleOverrides: {
         root: {
@@ -27,7 +27,7 @@ const theme = createTheme({
     },
     MuiSvgIcon: {
       defaultProps: {
-        className: 'absolute left-5'
+        className: 'absolute left-5',
       }
     }
   }
@@ -40,26 +40,26 @@ function GeneralSignin() {
         <Typography variant="h4" className="my-16">Sign in to Tutoring Scheduler</Typography>
         <Stack spacing={3}>
           <ThemeProvider theme={theme}>
-            <Button sx={{ backgroundColor: '#10B981' }}>
+            <Button sx={{ backgroundColor: '#10B981', ":hover": { backgroundColor: '#10B981'}}}>
               <SvgIcon viewBox='0 0 41 41'>
                 <EmailIcon />
               </SvgIcon>
               Sign in with Email
             </Button>
-            <Button sx={{ backgroundColor: '#3B82F6' }}>
+            <Button sx={{ backgroundColor: '#3B82F6', ":hover": { backgroundColor: '#3B82F6' }}}>
               <SvgIcon viewBox='0 0 31 31'>
                 <EllipseIcon />
                 <GoogleIcon />
               </SvgIcon>
               Sign in with Google
             </Button>
-            <Button sx={{ backgroundColor: '#0284C7' }}>
+            <Button sx={{ backgroundColor: '#0284C7', ":hover": { backgroundColor: '#0284C7' } }}>
               <SvgIcon viewBox='0 0 40 40'>
                 <FacebookIcon />
               </SvgIcon>
               Sign in with Facebook
             </Button>
-            <Button sx={{ backgroundColor: '#F5F5F5' }} className="text-[#191919]">
+            <Button sx={{ backgroundColor: '#F5F5F5', ":hover": { backgroundColor: '#F5F5F5' } }} className="text-[#191919]">
               <SvgIcon viewBox='0 0 44 44'>
                 <AppleIcon />
               </SvgIcon>
