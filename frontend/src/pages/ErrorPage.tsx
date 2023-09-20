d5a527f7ed0b34e3199856bcb1bb3b3e57a6bda5
Anthony Jaramillo <anthony.jaramillomartinez@gmail.com>
2023-09-20 10:09:18 -0500

    copy pasta no good

--- frontend/src/pages/ErrorPage.tsx
+++ frontend/src/pages/ErrorPage.tsx
@@ -2,7 +2,7 @@ import Box from '@mui/material/Box';
 import Header from '../components/Header'
 import SvgIcon from '@mui/material/SvgIcon';
 import ErrorIcon from '../assets/icons/404-Icon.svg';
-import { Typography } from '@mui/material';
+import Typography from '@mui/material/Typography';
 
 function ErrorPage() {
   return (
@@ -13,7 +13,7 @@ function ErrorPage() {
           <ErrorIcon />
         </SvgIcon>
         <Typography variant="h4" className="my-6">Page not found!</Typography>
-        <Typography variant="h6">Sorry, we couldn’t find what you were looking for.</Typography>
+        <Typography variant="h6">Sorry, we couldn't find what you were looking for.</Typography>
       </Box>
     </>
   )
