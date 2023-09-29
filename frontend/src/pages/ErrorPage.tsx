593c6e624aafb0d5a6c97aa0d78db89ed96200c2
Anthony Jaramillo <anthony.jaramillomartinez@gmail.com>
2023-09-29 14:20:34 -0500

    Better indentation for readability

--- frontend/src/pages/ErrorPage.tsx
+++ frontend/src/pages/ErrorPage.tsx
@@ -9,7 +9,9 @@ function ErrorPage() {
     <>
       <Header />
       <Box className="grid justify-items-center bg-[#191919]">
-        <SvgIcon viewBox="0 0 160 160" className="w-[160px] h-[160px] mt-40">
+        <SvgIcon
+          viewBox="0 0 160 160"
+          className="w-[160px] h-[160px] mt-40">
           <ErrorIcon />
         </SvgIcon>
         <Typography variant="h4" className="my-6">Page not found!</Typography>
