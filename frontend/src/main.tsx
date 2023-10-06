90a700340504573ca88a3891aae2f221a3fd1d8a
Anthony Jaramillo <anthony.jaramillomartinez@gmail.com>
2023-10-06 09:54:40 -0500

    random comma removal

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -12,5 +12,5 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
       <CssBaseline />
       <App />
     </ ThemeProvider>
-  </React.StrictMode>,
+  </React.StrictMode>
 );
