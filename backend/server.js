00fa05102b060655ad0556e24f9132ef58ba596d
alymuhammad.damani@gmail.com <alymuhammad.damani@gmail.com>
2023-10-10 10:48:14 -0500

    register for backend added to server.js

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,59 @@ app.use(express.json());
 
 // Basic route
 app.get('/', (req, res) => {
-  res.json({ message: 'Hello, world!' });
+  res.sendFile(__dirname + '/EmailSignIn.tsx');
+})
+
+app.post('/register', encodeUrl, (req, res) => {
+  var firstName = req.body.firstName;
+  var lastName = req.body.lastName;
+  var userName = req.body.userName;
+  var password = req.body.password;
+
+  confirm.connect(function(err) {
+    if (err)
+    {
+      console.log(err);
+    }
+
+    // check if already registered
+    con.query(`SELECT * FROM users WHERE username = '${userName}'`, function(err, result)
+    {
+      if (err)
+      {
+        console.log(err);
+      }
+      else
+      {
+        // redirect to success page
+        function toSuccessRegister()
+        {
+          req.session.user = {
+            user_id: user_id,
+            email: email,
+            username: userName,
+            password: password,
+            user_type: user_type,
+            total_tutoring_hours: total_tutoring_hours
+          };
+
+        }
+
+        var new_user_SQL = `INSERT INTO users (user_id, email, username, password, user_type, total_tutoring_hours) VALUES ('${user_id}', '${email}', '${userName}', '${password}', '${user_type}', '${total_tutoring_hours}')`;
+        con.query(sql, function(err, result)
+        {
+          if (err)
+          {
+            console.log(err);
+          }
+          else
+          {
+            toSuccessRegister();
+          }
+        })
+      }
+    })
+  })
 });
 
 // Start server
