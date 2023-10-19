b2d5b5186f8595d7674fe2513aab8e8996d41520
Anthony Jaramillo <anthony.jaramillomartinez@gmail.com>
2023-10-18 23:50:18 -0500

    MFA UI setup

--- frontend/src/pages/EmailSignIn.tsx
+++ frontend/src/pages/EmailSignIn.tsx
@@ -8,74 +8,79 @@ import { ThemeProvider } from '@emotion/react';
 import { squareButtonTheme, checkboxTheme, textFieldTheme } from '../theme';
 import { useState } from 'react';
 import { createTheme } from '@mui/material';
+import MultifactorAuth from '../components/MultifactorAuth';
 
-const theme = createTheme(textFieldTheme, checkboxTheme, squareButtonTheme)
+const theme = createTheme(textFieldTheme, checkboxTheme, squareButtonTheme);
 
 function EmailSignin() {
   const [email, setEmail] = useState<string>('');
   const [password, setPassword] = useState<string>('');
+  const [showMFAuth, setShowMFAuth] = useState<boolean>(false);
 
   const formSubmitHandler = (
     e: React.FormEvent<EventTarget>
   ) => {
     e.preventDefault();
-    console.log(email, password)
+    console.log(email, password);
+    setShowMFAuth(true);
   }
 
   return (
     <>
-      <form onSubmit={formSubmitHandler}>
-        <Box className="grid justify-center bg-[#191919]">
-          <Typography
-            variant="h4"
-            className="mt-24 mb-16 justify-self-center">
-            Sign in
-          </Typography>
-          <ThemeProvider theme={theme}>
-            <TextField
-              required
-              id="email"
-              name="email"
-              label="Required"
-              placeholder="Email Address"
-              autoComplete="off"
-              className="w-[410px] mb-10"
-              onChange={e => setEmail(e.target.value)} />
-            <TextField
-              required
-              id="password"
-              name="password"
-              label="Required"
-              placeholder="Password"
-              type="password"
-              className="w-[410px] mb-4"
-              onChange={e => setPassword(e.target.value)} />
-            <Button
-              className="mt-8 py-2"
-              type="submit">
-              SIGN IN
-            </Button>
-            <div className='flex justify-between'>
-              <Link
-                to="/password-reset"
-                component={RouterLink}
-                className="mt-2"
-                color="#A3A3A3"
-                fontSize={14}>
-                Forgot Password?
-              </Link>
-              <Link
-                to="/signup"
-                component={RouterLink}
-                className="mt-2"
-                color="#A3A3A3"
-                fontSize={14}>
-                Don't have an account? Sign up
-              </Link>
-            </div>
-          </ThemeProvider>
-        </Box>
-      </form >
+      <Box className="grid justify-center bg-[#191919]">
+        {!showMFAuth ?
+          <form className="grid" onSubmit={formSubmitHandler}>
+            <Typography
+              variant="h4"
+              className="mt-24 mb-16 justify-self-center">
+              Sign in
+            </Typography>
+            <ThemeProvider theme={theme}>
+              <TextField
+                required
+                id="email"
+                name="email"
+                label="Required"
+                placeholder="Email Address"
+                autoComplete="off"
+                className="w-[410px] mb-10"
+                onChange={e => setEmail(e.target.value)} />
+              <TextField
+                required
+                id="password"
+                name="password"
+                label="Required"
+                placeholder="Password"
+                type="password"
+                className="w-[410px] mb-4"
+                onChange={e => setPassword(e.target.value)} />
+              <Button
+                className="mt-8 py-2"
+                type="submit">
+                SIGN IN
+              </Button>
+              <div className='flex justify-between'>
+                <Link
+                  to="/password-reset"
+                  component={RouterLink}
+                  className="mt-2"
+                  color="#A3A3A3"
+                  fontSize={14}>
+                  Forgot Password?
+                </Link>
+                <Link
+                  to="/signup"
+                  component={RouterLink}
+                  className="mt-2"
+                  color="#A3A3A3"
+                  fontSize={14}>
+                  Don't have an account? Sign up
+                </Link>
+              </div>
+            </ThemeProvider>
+          </form >
+          : <MultifactorAuth />}
+      </Box>
     </>
   )
 }
