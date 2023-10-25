6d15645dd9e8e7e7127a4f3cd87cdb9572c05a88
Anthony Jaramillo <anthony.jaramillomartinez@gmail.com>
2023-10-25 15:05:41 -0500

    Refactoring signup form inputs

--- frontend/src/pages/StudentSignup.tsx
+++ frontend/src/pages/StudentSignup.tsx
@@ -1,23 +1,14 @@
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
-import TextField from '@mui/material/TextField';
+import { Form } from 'react-router-dom';
 import Button from '@mui/material/Button';
 import Link from '@mui/material/Link';
-import { Link as RouterLink, Form } from 'react-router-dom';
+import { Link as RouterLink } from "react-router-dom";
+import GeneralSignupInfo from '../components/GeneralSignupInfo';
+import { squareButtonTheme } from '../theme';
 import { ThemeProvider } from '@emotion/react';
-import { squareButtonTheme, checkboxTheme, textFieldTheme } from '../theme';
-import { useState } from 'react';
-import { createTheme } from '@mui/material';
 
-const theme = createTheme(textFieldTheme, checkboxTheme, squareButtonTheme);
-
-function StudentSignup() {
-  const [firstName, setFirstName] = useState<string>('');
-  const [lastName, setLastName] = useState<string>('');
-  const [email, setEmail] = useState<string>('');
-  const [password, setPassword] = useState<string>('');
-  const [phoneNumber, setPhoneNumber] = useState<string>('');
-
+const StudentSignup = () => {
   return (
     <>
       <Form method="post">
@@ -27,57 +18,8 @@ function StudentSignup() {
             className='mt-8 mb-10 justify-self-center'>
             Sign up
           </Typography>
-          <ThemeProvider theme={theme}>
-            <TextField
-              required
-              id='first-name'
-              name='first-name'
-              label='Required'
-              placeholder='First Name'
-              autoComplete='off'
-              className='w-[410px] mb-10'
-              onChange={(e) => setFirstName(e.target.value)}
-            />
-            <TextField
-              required
-              id='last-name'
-              name='last-name'
-              label='Required'
-              placeholder='Last Name'
-              autoComplete='off'
-              className='w-[410px] mb-10'
-              onChange={(e) => setLastName(e.target.value)}
-            />
-            <TextField
-              required
-              id='email'
-              name='email'
-              label='Required'
-              placeholder='Email Address'
-              autoComplete='off'
-              className='w-[410px] mb-10'
-              onChange={(e) => setEmail(e.target.value)}
-            />
-            <TextField
-              required
-              id='phone-number'
-              name='phone-number'
-              label='Required'
-              placeholder='Phone Number'
-              type='tel'
-              className='w-[410px] mb-10'
-              onChange={(e) => setPhoneNumber(e.target.value)}
-            />
-            <TextField
-              required
-              id='password'
-              name='password'
-              label='Required'
-              placeholder='Password'
-              type='password'
-              className='w-[410px] mb-4'
-              onChange={(e) => setPassword(e.target.value)}
-            />
+          <GeneralSignupInfo />
+          <ThemeProvider theme={squareButtonTheme}>
             <Button
               className='mt-4 py-2'
               type='submit'>
