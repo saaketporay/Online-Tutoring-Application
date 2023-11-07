9e116ae3f60e809222589a44301f2a3c39893fcb
Kuazk <allenz@m1-mac-allen.lan>
2023-11-07 00:37:18 -0600

    function call update

--- backend/src/models/Availability.js
+++ backend/src/models/Availability.js
@@ -1,13 +1,42 @@
 const { User, Tutor, Subject, Tutor_Subject, Tutor_Availability } = require('./index');
 
 const Availability = {
+
+  // not yet tested
   getAllTimesByTutorId: async (tutorId) => {
-    // Get all times for a tutor
+    // Get all times for a tutor by tutor id
+    try {
+      const times = await Tutor_Availability.findAll({
+        where: {
+          tutor_id: tutorId
+        }
+      });
+      return times;
+    } catch (err) {
+      return err;
+    }
   },
-  
+  // not yet tested
   getTutorNameByTutorId: async (tutorId) => {
     // Get tutor name by tutor id
-    },
+    try {
+      const tutor = await Tutor.findOne({
+        where: { tutor_id: tutorId },
+        include: [{
+          model: User,
+          attributes: ['first_name', 'last_name'],
+        }]
+      });
+      if (tutor && tutor.User) {
+        console.log(tutor.User.first_name, tutor.User.last_name);
+        return `${tutor.User.first_name} ${tutor.User.last_name}`;
+      }
+      return null;
+    } catch (err) {
+      console.error('Error fetching tutor name:', err);
+      throw err;
+    }
+  },
 
   getAllSubjects: async () => {
     try {
