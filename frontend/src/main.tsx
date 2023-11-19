c7142888e24f83a06701a18e16e3d014fc646082
ghp_W4ZlWmPs6RzjFKEX29t9kMam57qAtj2 <anthony.jaramillomartinez@gmail.com>
2023-11-19 14:15:46 -0600

    trying out hard coded url

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -9,7 +9,8 @@ import { Provider } from 'react-redux';
 import { store, persistor } from './redux/store';
 import { PersistGate } from 'redux-persist/integration/react';
 
-await fetch('google.com')
+const response = await fetch('100.24.50.65/availability/subjects')
+console.log(response);
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
