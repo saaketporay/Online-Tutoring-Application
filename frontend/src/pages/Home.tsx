eda68f37f4859457c41b97fbcae30eeb1747491f
ghp_W4ZlWmPs6RzjFKEX29t9kMam57qAtj2 <anthony.jaramillomartinez@gmail.com>
2023-11-19 14:40:40 -0600

    Title change

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -13,7 +13,7 @@ const Home = () => {
         <Typography
           variant='h4'
           className='mt-36 mb-5'>
-          Welcome to Online Tutoring Scheduler!
+          Welcome!
         </Typography>
         <Typography
           variant='h6'
