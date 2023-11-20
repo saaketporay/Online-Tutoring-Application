782547513fc3db28bbdd75423a24b8bd53ddcf98
ghp_W4ZlWmPs6RzjFKEX29t9kMam57qAtj2 <anthony.jaramillomartinez@gmail.com>
2023-11-19 18:57:55 -0600

    fix port

--- frontend/src/utils/axios.ts
+++ frontend/src/utils/axios.ts
@@ -5,7 +5,7 @@ export const axiosInstance = () => {
   const token = store.getState().auth.token;
   // console.log(token)
   const instance = axios.create({
-    baseURL: `http://${import.meta.env.VITE_BACKEND_BASE_URL}:${import.meta.env.BACKEND_PORT}`,
+    baseURL: `http://${import.meta.env.VITE_BACKEND_BASE_URL}:${import.meta.env.VITE_BACKEND_PORT}`,
     headers: {
       'Content-Type': 'application/json',
       'Authorization': `Bearer ${token}`,
