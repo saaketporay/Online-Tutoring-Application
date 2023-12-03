4587e382490a863166e7d178f27efd22aedd4927
Anthony Jaramillo <anthony.jaramillomartinez@gmail.com>
2023-12-03 11:53:02 -0600

    matching upstream axios config

--- frontend/src/utils/axios.ts
+++ frontend/src/utils/axios.ts
@@ -4,7 +4,7 @@ import { store } from '../redux/store';
 export const axiosInstance = (sendingImg = false) => {
   const token = store.getState().auth.token;
   const instance = axios.create({
-    baseURL: `http://${import.meta.env.VITE_BACKEND_BASE_URL}:${import.meta.env.VITE_BACKEND_PORT}`,
+    baseURL: import.meta.env.VITE_BACKEND_BASE_URL,
     headers: {
       'Content-Type': sendingImg ? 'multipart/form-data' : 'application/json',
       Authorization: `Bearer ${token}`,
