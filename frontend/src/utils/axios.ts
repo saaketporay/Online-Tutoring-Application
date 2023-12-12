2ef183aa9b029a26b0b6869e43d40f0ba6376b7c
Anthony A Jaramillo-Martinez <90576643+anthonyjara7@users.noreply.github.com>
2023-12-11 18:41:25 -0600

    Revert "Compose update"

--- frontend/src/utils/axios.ts
+++ frontend/src/utils/axios.ts
@@ -4,7 +4,7 @@ import { store } from '../redux/store';
 export const axiosInstance = (sendingImg = false) => {
   const token = store.getState().auth.token;
   const instance = axios.create({
-    baseURL: `http://${import.meta.env.VITE_BACKEND_BASE_URL}:3000`,
+    baseURL: import.meta.env.VITE_BACKEND_BASE_URL,
     headers: {
       'Content-Type': sendingImg ? 'multipart/form-data' : 'application/json',
       Authorization: `Bearer ${token}`,
